aa6bee8146165d6f5da9770cd67b2790507b070f
Cameron Alexander <emptyflash@gmail.com>
2015-12-29 15:53:09 -0600

    Add clj-http dependecy

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]]
   :main ^:skip-aot clojurist-just-want-to-have-puns.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojurist_just_want_to_have_puns/core.clj
+++ src/clojurist_just_want_to_have_puns/core.clj
@@ -1,7 +1,8 @@
 (ns clojurist-just-want-to-have-puns.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as client]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (client/get "http://rhymebrain.com/talk?function=getRhymes&word=heart")))
