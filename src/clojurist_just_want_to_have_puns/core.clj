b88b5e826a68b962a13413c87e77d6d7810c4738
Cameron Alexander <emptyflash@gmail.com>
2015-12-29 16:03:49 -0600

    Fixed typo in packages too

--- src/clojurist_just_want_to_have_puns/core.clj
+++
@@ -1,8 +0,0 @@
-(ns clojurists-just-want-to-have-puns.core
-  (:gen-class)
-  (:require [clj-http.client :as client]))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println (client/get "http://rhymebrain.com/talk?function=getRhymes&word=heart")))
--- test/clojurist_just_want_to_have_puns/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojurists-just-want-to-have-puns.core-test
-  (:require [clojure.test :refer :all]
-            [clojurist-just-want-to-have-puns.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
